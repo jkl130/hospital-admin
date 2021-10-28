930b9a9458d75130867e88d78f2876a2debcfc87
jkl130 <842081147@qq.com>
2021-10-28 16:08:49 +0800

    111

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -1,11 +1,10 @@
-
 <template>
   <el-menu class="navbar" mode="horizontal">
     <hamburger class="hamburger-container" :toggleClick="toggleSideBar" :isActive="sidebar.opened"></hamburger>
     <breadcrumb></breadcrumb>
     <el-dropdown class="avatar-container" trigger="click">
       <div class="avatar-wrapper">
-        <img class="user-avatar" src="/static/imgs/logo1.png" />
+        <img class="user-avatar" src="/static/imgs/logo.jpg"/>
       </div>
       <el-dropdown-menu class="user-dropdown" slot="dropdown">
         <router-link class="inlineBlock" style="text-align: center;" to="/">
@@ -20,7 +19,7 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import {mapGetters} from "vuex";
 import Breadcrumb from "@/components/Breadcrumb";
 import Hamburger from "@/components/Hamburger";
 
@@ -52,30 +51,36 @@ export default {
   height: 50px;
   line-height: 50px;
   border-radius: 0px !important;
+
   .hamburger-container {
     line-height: 58px;
     height: 50px;
     float: left;
     padding: 0 10px;
   }
+
   .screenfull {
     position: absolute;
     right: 90px;
     top: 16px;
     color: red;
   }
+
   .avatar-container {
     height: 50px;
     display: inline-block;
     position: absolute;
     right: 35px;
+
     .avatar-wrapper {
       cursor: pointer;
       margin-top: 5px;
       position: relative;
+
       .user-avatar {
         width: 100px;
       }
+
       .el-icon-caret-bottom {
         position: absolute;
         right: -20px;
@@ -85,15 +90,64 @@ export default {
     }
   }
 }
-.pw-strength {clear: both;position: relative;top: 8px;width: 180px;}
-.pw-bar{background: url(/static/imgs/pwd-1.png) no-repeat;height: 14px;overflow: hidden;width: 179px;}
-.pw-bar-on{background:  url(/static/imgs/pwd-2.png) no-repeat; width:0px; height:14px;position: absolute;top: 1px;left: 2px;transition: width .5s ease-in;-moz-transition: width .5s ease-in;-webkit-transition: width .5s ease-in;-o-transition: width .5s ease-in;}
-.pw-weak .pw-defule{ width:0px;}
-.pw-weak .pw-bar-on {width: 60px;}
-.pw-medium .pw-bar-on {width: 120px;}
-.pw-strong .pw-bar-on {width: 179px;}
-.pw-txt {padding-top: 2px;width: 180px;overflow: hidden;}
-.pw-txt span {color: #707070;float: left;font-size: 12px;text-align: center;width: 58px;}
+
+.pw-strength {
+  clear: both;
+  position: relative;
+  top: 8px;
+  width: 180px;
+}
+
+.pw-bar {
+  background: url(/static/imgs/pwd-1.png) no-repeat;
+  height: 14px;
+  overflow: hidden;
+  width: 179px;
+}
+
+.pw-bar-on {
+  background: url(/static/imgs/pwd-2.png) no-repeat;
+  width: 0px;
+  height: 14px;
+  position: absolute;
+  top: 1px;
+  left: 2px;
+  transition: width .5s ease-in;
+  -moz-transition: width .5s ease-in;
+  -webkit-transition: width .5s ease-in;
+  -o-transition: width .5s ease-in;
+}
+
+.pw-weak .pw-defule {
+  width: 0px;
+}
+
+.pw-weak .pw-bar-on {
+  width: 60px;
+}
+
+.pw-medium .pw-bar-on {
+  width: 120px;
+}
+
+.pw-strong .pw-bar-on {
+  width: 179px;
+}
+
+.pw-txt {
+  padding-top: 2px;
+  width: 180px;
+  overflow: hidden;
+}
+
+.pw-txt span {
+  color: #707070;
+  float: left;
+  font-size: 12px;
+  text-align: center;
+  width: 58px;
+}
+
 .notification {
   display: flex;
   height: inherit;
@@ -104,14 +158,17 @@ export default {
   right: 180px;
   top: 0;
 }
+
 .notification-icon {
   cursor: pointer;
   font-size: 20px;
   transform: rotate(45deg);
 }
+
 .notification-icon:hover {
   color: rgb(0, 82, 204);
 }
+
 .notification-count {
   position: absolute;
   left: 17px;
